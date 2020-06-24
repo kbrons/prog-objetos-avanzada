e2123fb932fd677c393c4899745d68b47c3fb27d
Kevin <bronsteinkevin@gmail.com>
2020-06-24 11:01:00 -0300

    Add docstrings to model

--- AOOPMessages/models.py
+++ AOOPMessages/models.py
@@ -1,3 +1,8 @@
+"""AOOPMessages models module.
+
+This module contains the classes representing the application model.
+"""
+
 from AOOPMessages import db
 from AOOPMessages import login_manager
 from datetime import datetime
@@ -7,10 +12,43 @@ from sqlalchemy.orm import relationship
 
 @login_manager.user_loader
 def load_user(user_id):
+    """Load User for Login Manager implementation.
+
+    Method to retrieve the current_user for the Login Manager module.
+
+    Parameters
+    ----------
+        user_id : int
+            Id of the logged in user.
+
+    Returns
+    -------
+        User
+            User instance of the logged in user.
+
+    .. _PEP 484:
+        https://www.python.org/dev/peps/pep-0484/
+
+    """
+
     return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
+    """User class.
+
+    Class defining the user model for the application.
+
+    Attributes
+    ----------
+        id : int
+            Id of the user.
+        email : str
+            Email of the user.
+        password : str
+            Hashed password of the user.
+
+    """
 
     __tablename__ = 'users'
 
@@ -23,6 +61,30 @@ class User(db.Model, UserMixin):
 
 
 class Message(db.Model):
+    """Message class.
+
+    Class defining the message model for the application.
+
+    Attributes
+    ----------
+        id : int
+            Id of the message.
+        title : str
+            Title of the message.
+        Body : str
+            Body of the message.
+        timestamp : DateTime
+            Date and time when the message was sent.
+        author_id : int
+            Id of the user that sent the message.
+        receiver_id : int
+            Id of the user that received the message.
+        author : User
+            User that sent the message.
+        receiver : User
+            User that received the message.
+
+    """
 
     __tablename__ = 'messages'
 
